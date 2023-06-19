1ae9de83487955a79fdd21cd8008f9c5a3c659a9
kisyam <how2logout@gmail.com>
2023-06-20 00:36:28 +0900

    Questions index

---
+++ lib/data/questions.dart
@@ -0,0 +1,40 @@
+import 'package:adv_basics/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    "What are the main building blocks of Flutter UIs?",
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestion(
+    "Which type of widgets in Flutter can change and maintain their state?",
+    [
+      'Stateful widgets',
+      'Stateless widgets',
+      'Functional widgets',
+      'Component widgets',
+    ],
+  ),
+  QuizQuestion(
+    "Which Flutter widgets are used for arranging and organizing other widgets on the screen?",
+    [
+      'Layout widgets',
+      'Container widgets',
+      'Text widgets',
+      'Input widgets',
+    ],
+  ),
+  QuizQuestion(
+    "Which Flutter widget is used for displaying text on the screen?",
+    [
+      'TextBlock',
+      'TextView',
+      'TextWidget',
+      'Text',
+    ],
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
-import 'package:adv_basics/start_screen.dart';
+import 'package:adv_basics/quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const QuizApp());
-}
-
-class QuizApp extends StatelessWidget {
-  const QuizApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 78, 13, 151),
-                Color.fromARGB(255, 107, 15, 168),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
